77bca4cab1d544dd76e4b7c201807b45f3c89483
TS-Liu <1347481725@qq.com>
2018-05-09 20:52:32 +0800

    Default

--- onmt/modules/Transformer.py
+++ onmt/modules/Transformer.py
@@ -283,7 +283,7 @@ class TransformerDecoder(nn.Module):
         decoder_local_mask = attention.get_local_mask(tgt_len) #[1, length, length]
         decoder_local_mask = decoder_local_mask.repeat(tgt_batch, 1, 1)
         decoder_bias = torch.gt(tgt_pad_mask + decoder_local_mask, 0).float() * -1e9
-        output_mask = torch.unsqueeze(tgt_unk_mask * -1e9, 1).transpose(0,2).repeat(1, hiden_len, 1).transpose(1,2)
+        output_mask = (tgt_unk_mask * -1e9).transpose(0,2).repeat(1, hiden_len, 1).transpose(1,2)
 
         saved_inputs = []
         for i in range(self.num_layers):
@@ -400,7 +400,7 @@ class Unk_TransformerDecoder(nn.Module):
         # unk_bias = torch.unsqueeze(unk_bias * -1e9, 1)   ########
         encoder_decoder_bias = torch.unsqueeze(src_pad_mask * -1e9, 1)
         decoder_bias = torch.gt(tgt_pad_mask + tgt_unk_mask, 0).float() * -1e9
-        output_mask = torch.unsqueeze(src_pad_mask * -1e9, 1).transpose(0,2).repeat(1, hiden_len, 1).transpose(1,2)
+        output_mask = (src_pad_mask * -1e9, 1).transpose(0,2).repeat(1, hiden_len, 1).transpose(1,2)
         saved_inputs = []
         for i in range(self.num_layers):
             prev_layer_input = None
