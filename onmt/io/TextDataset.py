c7d07edc6e14228b6a2f2526adae67268730d10f
TS-Liu <1347481725@qq.com>
2018-05-10 22:14:16 +0800

    Default

--- onmt/io/TextDataset.py
+++ onmt/io/TextDataset.py
@@ -482,10 +482,11 @@ class Field(torchtext.data.Field):
                 for a in arr_big:
                     if len(a)>max_len :
                         max_len=len(a)
-                N = numpy.ones([len(arr_small),max_len]).astype(int).tolist()
+                N = numpy.ones([len(arr_small),max_len+1]).astype(int).tolist()
                 i=0
                 for a in arr_big:
-                    N[i][:len(a)]=a
+                    N[i][1:len(a)]=a
+                    N[i][0] = 2
                     i=i+1
                 N=list(N)
                 arrs.append(arr_small)
